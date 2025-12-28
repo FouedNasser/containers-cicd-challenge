f4ba1a1c4ca4400850cb544cf96027f1fbdfd9da
Foued Nasser <foued.nasser@ensi-uma.tn>
2025-12-28 14:10:46 +0100

    feat: implement password hashing and jwt generation

---
+++ app/auth.py
@@ -0,0 +1,22 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+from .config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Hash:
+    @staticmethod
+    def bcrypt(password: str):
+        return pwd_context.hash(password)
+
+    @staticmethod
+    def verify(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    return encoded_jwt
